7d6249b81f6aba5ad59557e93b75e5a46d1c3669
Sonali Mergal <91054291+mergal075@users.noreply.github.com>
2023-04-04 17:31:25 +0530

    Add files via upload

---
+++ HelloConcat.java
@@ -0,0 +1,14 @@
+package com.edu;
+
+public class HelloConcat {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello"+2+3);
+		System.out.println(2+3+"Hello");
+		System.out.println("Hello"+(2+3));
+		System.out.println("2+3+Hello");
+	}
+
+}
