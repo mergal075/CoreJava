53a4c58836bcf3f7d39e0846d2a8ae3059428360
Sonali Mergal <91054291+mergal075@users.noreply.github.com>
2023-04-04 17:31:04 +0530

    Add files via upload

---
+++ Increment.java
@@ -0,0 +1,24 @@
+package com.edu;
+import java.util.*;
+public class Increment {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter the Name");
+		String name=sc.nextLine();
+		System.out.println("enter the age");
+		int age=sc.nextInt();
+		System.out.println("enter center");
+		String center=sc.next();
+		System.out.println("enter the degree");
+		sc.nextLine();
+		String degree=sc.nextLine();
+		System.out.println("enter the average mark");
+		int marks=sc.nextInt();
+		
+		System.out.println("Name"+name+" \n age "+age+" \n center name "+center+"  degree  "+degree+"  average marks "+marks);
+		
+		
+	}
+}	
