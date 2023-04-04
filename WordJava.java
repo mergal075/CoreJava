bd3987c556ee7a84ee80744bc385804475568a06
Sonali Mergal <91054291+mergal075@users.noreply.github.com>
2023-04-04 17:37:43 +0530

    Add files via upload

---
+++ WordJava.java
@@ -0,0 +1,13 @@
+package com.edu;
+
+public class WordJava {
+
+	public static void main(String[] args) {
+		System.out.println("   J        a     V      V     a     ");
+		System.out.println("   J     a     a   V     V  a     a ");
+		System.out.println("   J    aaaaaaaaa   V   V   aaaaaaa  ");
+		System.out.println("J  J    a       a    V V    a     a  ");
+	    System.out.println("JJJJ    a       a     V     a     a  ");
+	}
+
+}
