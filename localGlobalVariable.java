e6ba2e0037e6751b27d9fc9aa1900fb37c5dcd02
Sonali Mergal <91054291+mergal075@users.noreply.github.com>
2023-04-04 17:31:54 +0530

    Add files via upload

---
+++ localGlobalVariable.java
@@ -0,0 +1,11 @@
+package com.edu;
+
+public class localGlobalVariable {
+
+	public static void main(String[] args) {
+		int a=5;
+		System.out.println(a); //It give compilation error because we are not initialized a=5
+		
+	}
+
+}
