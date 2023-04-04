7e1e04d049cab39142d21f1381d21e7426f34de1
Sonali Mergal <91054291+mergal075@users.noreply.github.com>
2023-04-04 17:27:48 +0530

    Add files via upload

---
+++ AmericanFlag.java
@@ -0,0 +1,19 @@
+package com.edu;
+
+public class AmericanFlag {
+
+	public static void main(String[] args) {
+		for(int i=0;i<8;i++)
+		{
+			
+		
+				System.out.println("******====================");
+			
+		}
+		for(int j=0;j<5;j++)
+		{
+			System.out.println("=========================");
+		}
+	}
+
+}
