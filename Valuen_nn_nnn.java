3bf340c779575fa041548aed387008c1a3f1d10d
Sonali Mergal <91054291+mergal075@users.noreply.github.com>
2023-04-04 17:37:18 +0530

    Add files via upload

---
+++ Valuen_nn_nnn.java
@@ -0,0 +1,12 @@
+package com.edu;
+
+public class Valuen_nn_nnn {
+
+	public static void main(String[] args) {
+		int num=5;
+		System.out.println(+num+" + "+num+num+" + "+num+num+num);
+		
+
+	}
+
+}
