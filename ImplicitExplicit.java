9a198c7312e7e1000bfc4f6cd1621075ddf97fcc
Sonali Mergal <91054291+mergal075@users.noreply.github.com>
2023-04-04 17:39:19 +0530

    Add files via upload

---
+++ ImplicitExplicit.java
@@ -0,0 +1,18 @@
+package com.edu;
+import java.util.*;
+public class ImplicitExplicit {
+
+	public static void main(String[] args) {
+			
+			long l=122345;
+			int d=(int)l;
+			System.out.println(d);
+
+			int a=10;
+			short s=(short)a;
+			System.out.println(s);
+			
+			
+	}
+
+}
