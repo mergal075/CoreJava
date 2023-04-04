474fefc2a002d69cd4a8de2a174db2676a4a4b93
Sonali Mergal <91054291+mergal075@users.noreply.github.com>
2023-04-04 17:35:03 +0530

    Add files via upload

---
+++ SpaceTwinkle.java
@@ -0,0 +1,11 @@
+package com.edu;
+
+public class SpaceTwinkle {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Twinkle Twinkle little star,\n\t How I wonder what you are!,\n\t\t Up above the world so high, \n\t\tLike a diamond in the sky.\nTwinkle Twinkle little star\n\t How I wonder what you are!");
+
+	}
+
+}
